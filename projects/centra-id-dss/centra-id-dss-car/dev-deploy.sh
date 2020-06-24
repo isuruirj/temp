79e79a7edcc2d46f9de31b0c1eb0dc287cd1ec55
Anupam Gogoi <anupam@wso2.com>
2020-06-24 13:52:31 -0300

    saas

--- projects/centra-id-dss/centra-id-dss-car/dev-deploy.sh
+++
@@ -1,5 +0,0 @@
-cd ../centra-id-dss
-mvn clean install
-cd ../centra-id-dss-car
-mvn clean install
-sshpass -p "Zv8#+gjpEukD" scp target/centra-id.dss-car_1.0.0.car atikasoft@atk10.centralus.cloudapp.azure.com:/opt/wso2ei-6.5.0/repository/deployment/server/carbonapps
--- projects/centra-id-ei-core/centra-id-integrator-car-develop/dev-deploy.sh
+++
@@ -1,5 +0,0 @@
-cd ../centra-id-micro-integrator
-mvn clean install -P desa-bp
-cd ../centra-id-integrator-car
-mvn clean install
-##sshpass -p "Zv8#+gjpEukD" scp target/centra-id.integrator-car_1.0.0.car atikasoft@atk10.centralus.cloudapp.azure.com:/opt/wso2ei-6.5.0/repository/deployment/server/carbonapps
